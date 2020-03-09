cff1428e7c7ec4a151c938af8330b6174b4e1b6a
Michael Usher <michael.usher@aarnet.edu.au>
2020-03-09 12:07:22 +1100

    comments for options

--- config.py
+++ config.py
@@ -1,17 +1,19 @@
 # copy values of these cookies for LJ from your browser after logging in
-
-archive_years = {
-    'start': 2000,
-    'end': 2011
-}
-
 cookies = {
     'ljloggedin': '',
     'ljmastersession': '',
 }
 
+# The start and end years of posts you would like to download.
+archive_years = {
+    'start': 2000,
+    'end': 2010
+}
+
+# User agent to present to the livejournal servers.
 headers = {
     'User-Agent': 'https://github.com/arty-name/livejournal-export; me@arty.name'
 }
 
+# Skip downloading and just process edata that has already been downloaded
 skip_download: False
